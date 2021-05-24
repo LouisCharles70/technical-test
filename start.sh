937d1cfe68f554cf4d6b8a061e665072f2dec6a9
Louis <louis.charles@gadz.org>
2021-05-24 19:42:06 +0800

    Start script updated: added env variables; install commands; migrations

--- start.sh
+++ start.sh
@@ -1,6 +1,15 @@
+cp .env.example .env
+echo 'DB_HOST=lalamove-database' >> .env
+echo 'DB_DATABASE=database' >> .env
+echo 'DB_USERNAME=laraveluser' >> .env
+echo 'DB_PASSWORD=root' >> .env
+
+
 docker-compose up --build -d
 
 #Wait for all the containers to be ready
 sleep 10
 
+docker exec lalamove-app composer install --no-interaction && npm install && php artisan key:generate
+sleep 2
 docker exec lalamove-app php artisan migrate
